522cc149d810ba8ef304692d1744633b5fc2c3ee
eladlavi <eladlavi@users.noreply.github.com>
2019-02-25 19:46:21 +0200

    changed the layout"

--- camelRecyclerView.java
+++ camelRecyclerView.java
@@ -1,8 +1,8 @@
-//please add a normal recycler view
+\//please add a normal recycler view
 //use the view holder with backendless system
 public class studentData{
 	public void onCreate (bundle savedInstance);
-setContentView(R.layout.main_activity);
+setContentView(R.layout.login);
 Backendless.initApp(this,APPLICATION_ID,API_KEY);
 }
 }
