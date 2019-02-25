689f5dd7adbbc85ae60fda89aed86d0ffe17951c
eladlavi <eladlavi@users.noreply.github.com>
2019-02-25 19:44:03 +0200

    fixed some issues

--- camelRecyclerView.java
+++ camelRecyclerView.java
@@ -1,3 +1,8 @@
 //please add a normal recycler view
 //use the view holder with backendless system
+public class studentData{
+	public void onCreate (bundle savedInstance);
+setContentView(R.layout.main_activity);
 Backendless.initApp(this,APPLICATION_ID,API_KEY);
+}
+}
